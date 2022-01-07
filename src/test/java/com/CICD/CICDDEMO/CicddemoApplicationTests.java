0373a86ae72a88e6b023d3dde8c983ca3da37521
Amit <you@example.com>
2022-01-07 16:59:47 +0530

    Initial code commit

---
+++ src/test/java/com/CICD/CICDDEMO/CicddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CICD.CICDDEMO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
